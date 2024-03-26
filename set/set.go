571959128639bd40bbcdecf6049628d0cd3ece92
gaoxinghua01 <gaoxinghua01@baidu.com>
2024-03-26 22:57:55 +0800

    增加对set的实现

---
+++ set/set.go
@@ -0,0 +1,39 @@
+package set
+
+type Set[T comparable] interface {
+	Add(key T)
+	Delete(key T)
+	Exist(key T) bool
+	Keys() []T
+}
+
+type MapSet[T comparable] struct {
+	m map[T]struct{}
+}
+
+func NewMapSet[T comparable](size int) *MapSet[T] {
+	return &MapSet[T]{
+		m: make(map[T]struct{}, size),
+	}
+}
+
+func (s *MapSet[T]) Add(val T) {
+	s.m[val] = struct{}{}
+}
+
+func (s *MapSet[T]) Delete(key T) {
+	delete(s.m, key)
+}
+
+func (s *MapSet[T]) Exist(key T) bool {
+	_, ok := s.m[key]
+	return ok
+}
+
+func (s *MapSet[T]) Keys() []T {
+	ans := make([]T, 0, len(s.m))
+	for key := range s.m {
+		ans = append(ans, key)
+	}
+	return ans
+}
