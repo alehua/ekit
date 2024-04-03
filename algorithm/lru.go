c47d54eb974a3651fe49b134f6f326185dcbdbc6
gaoxinghua01 <gaoxinghua01@baidu.com>
2024-04-03 20:47:41 +0800

    删除多余文件

--- algorithm/lru.go
+++
@@ -1,9 +0,0 @@
-package algorithm
-
-import "container/list"
-
-type keyLru struct {
-	limit    int                      //缓存数量
-	evicts   *list.List               //双向链表用于淘汰数据
-	elements map[string]*list.Element //记录缓存数据
-}
